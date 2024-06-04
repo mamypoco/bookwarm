2874d93731073d4666e27a47eafc00692c183b25
Mami. N <mamypoco@gmail.com>
2024-06-04 09:29:15 -0700

    manual register login works but after updating a book, the list won't show in the dashboard

--- src/components/Login/login.js
+++ src/components/Login/login.js
@@ -14,29 +14,41 @@ import Swal from "sweetalert2";
 const Login = ({ setIsAuthenticated }) => {
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
+  const [isRegistering, setIsRegistering] = useState(false);
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+
+    const submitter = e.nativeEvent.submitter;
+    if (submitter.name === "Login") {
+      handleLogin(e);
+    } else if (submitter.name === "Register") {
+      handleRegister(e);
+    }
+  };
 
   const handleLogin = async (e) => {
     e.preventDefault();
     try {
       await signInWithEmailAndPassword(auth, email, password);
+      setIsAuthenticated(true);
       Swal.fire({
         timer: 1500,
         showConfirmButton: false,
-        willOpen: () => {
-          Swal.showLoading();
-        },
-        willClose: () => {
-          setIsAuthenticated(true);
+        //   willOpen: () => {
+        //     Swal.showLoading();
+        //   },
+        //   willClose: () => {
+        //     setIsAuthenticated(true);
 
-          Swal.fire({
-            icon: "success",
-            title: "Successfully logged in!",
-            showConfirmButton: false,
-            timer: 1500,
-          });
-        },
+        //  Swal.fire({
+        icon: "success",
+        title: "Successfully logged in!",
+        // showConfirmButton: false,
+        //   timer: 1500,
       });
-      setIsAuthenticated(true);
+      //   },
+      // });
     } catch (err) {
       Swal.fire({
         timer: 1500,
@@ -59,6 +71,11 @@ const Login = ({ setIsAuthenticated }) => {
   const handleRegister = async (e) => {
     e.preventDefault();
     try {
+      //set isRegistering true
+      if (!isRegistering) {
+        setIsRegistering(true);
+        return;
+      }
       //prompt user to enter their name
       const { value: name } = await Swal.fire({
         title: "Enter your name",
@@ -76,7 +93,7 @@ const Login = ({ setIsAuthenticated }) => {
         allowOUtsideClick: false,
       });
       if (name) {
-        //create user with email and password
+        //if name exists, create user with email and password
         const { user } = await createUserWithEmailAndPassword(
           auth,
           email,
@@ -88,13 +105,13 @@ const Login = ({ setIsAuthenticated }) => {
         });
 
         //store user info in Firestore
-        //   await db.collection("users").doc(user.uid).set({
-        //     displayName: name,
-        //   });
         await setDoc(doc(db, "users", user.uid), {
           displayName: name,
           email: user.email,
         });
+        //   await db.collection("users").doc(user.uid).set({
+        //     displayName: name,
+        //   });
 
         setIsAuthenticated(true);
         Swal.fire({
@@ -104,24 +121,6 @@ const Login = ({ setIsAuthenticated }) => {
           timer: 1500,
         });
       }
-
-      // Swal.fire({
-      //   timer: 1500,
-      //   showConfirmButton: false,
-      //   willOpen: () => {
-      //     Swal.showLoading();
-      //   },
-      //   willClose: () => {
-      //     setIsAuthenticated(true);
-
-      //     Swal.fire({
-      //       icon: "success",
-      //       title: "Successfully registered and logged in!",
-      //       showConfirmButton: false,
-      //       timer: 1500,
-      //     });
-      //   },
-      // });
     } catch (error) {
       console.log(error);
     }
@@ -141,12 +140,7 @@ const Login = ({ setIsAuthenticated }) => {
     <div className="login-container">
       <h1>BookWarm</h1>
 
-      <form
-        className="login-form"
-        onSubmit={
-          document.activeElement.name === "Login" ? handleLogin : handleRegister
-        }
-      >
+      <form className="login-form" onSubmit={handleSubmit}>
         <button className="google-signin-button" onClick={loginWithGoogle}>
           Login With Google
         </button>
